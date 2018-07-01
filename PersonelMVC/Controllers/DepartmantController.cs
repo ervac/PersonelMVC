2a72c06980a08efadefa056e5d492d7f9820b2c4
huluc <ulucay.hce@gmail.com>
2018-07-01 05:25:53 +0300

    first commit

---
+++ PersonelMVC/Controllers/DepartmantController.cs
@@ -0,0 +1,79 @@
+﻿using PersonelMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonelMVC.Controllers
+{
+    public class DepartmantController : Controller
+    {
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            using (PersonelDbContext db = new PersonelDbContext())
+            {
+                List<Departmant> lstDepartman = db.Departmants.ToList();
+                return View(lstDepartman);
+            }
+            
+        }
+        public ActionResult Create()
+        {
+            return View("DepartmantForm",new Departmant());
+        }
+   
+        [HttpPost]
+        public ActionResult Save(Departmant departmant)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("DepartmantForm", departmant);
+            }
+
+            using (PersonelDbContext db = new PersonelDbContext())
+            {
+                if (departmant.Id==0)
+                db.Departmants.Add(departmant);
+                else
+                {
+                    var updatedDepartmant = db.Departmants.Find(departmant.Id);
+                    updatedDepartmant.Name = departmant.Name;
+                }
+                db.SaveChanges();              
+            }
+            return RedirectToAction("Index", "Departmant");
+        }
+        public ActionResult Update(int id)
+        {
+            using (PersonelDbContext db = new PersonelDbContext())
+            {
+                var departmant = db.Departmants.Find(id);
+                if (departmant == null)
+                    return HttpNotFound();
+                else
+                    return View("DepartmantForm", departmant);
+
+            }
+
+        }
+        public ActionResult Delete(int id)
+        {
+            using (PersonelDbContext db = new PersonelDbContext())
+            {
+                var departmant = db.Departmants.Find(id);
+                if (departmant == null)
+                    return HttpNotFound();
+                else
+                {
+                    db.Departmants.Remove(departmant);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+            }
+        }
+    }
+}
---
+++ PersonelMVC/scripts/custom.js
@@ -0,0 +1,34 @@
+﻿$(function () {
+    $("#tblDepartmants").on("click", ".btnDelDep", function () {
+        var id = $(this).data("id");
+        var btn = $(this);
+     
+        //bootbox.confirm("Silmek istediğinize emin misiniz?", function (result) {
+        //    if (result) {
+        //        $.ajax({
+        //            type: "GET",
+        //            url: "/Departmant/Delete/" + id,
+        //            success: function () {
+        //                btn.parent().parent().remove();
+        //            }
+        //        })
+        //    }
+        //})
+        if(confirm("Silmek istediğinize emin misiniz?"))           
+                $.ajax({
+                    type: "GET",
+                    url: "/Departmant/Delete/" + id,
+                    success: function () {
+                        btn.parent().parent().remove();
+                    }
+                })
+    })
+})
+function CheckDateTypeIsValid(dateelement)
+{
+    var value = $(dateelement).val();
+    if (value == '')
+        $(dateelement).valid(false); 
+    else
+        $(dateelement).valid();
+}
